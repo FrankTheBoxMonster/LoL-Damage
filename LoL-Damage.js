99e96582ad6c2a67f3db5a7dfc3d0bc67eb618a9
FrankTheBoxMonster <11337782+FrankTheBoxMonster@users.noreply.github.com>
2023-01-23 00:08:28 -0600

    Adding a disclaimer for imperfections in data and how to report them

--- LoL-Damage.js
+++ LoL-Damage.js
@@ -1,3 +1,8 @@
+var issuesDisclaimer = document.createElement("p");
+document.body.appendChild(issuesDisclaimer);
+issuesDisclaimer.innerHTML = "<b>THIS SYSTEM IS A WORK IN PROGRESS.</b>  It does <b>NOT</b> claim to be perfect, <b>BUT</b> it should be pretty close.<br>This is all the result of human research.  We're usually pretty good, but there's always room for errors.  You should always test to confirm things before risking your ranked games.<br>If something seems missing, nonsensical, or wrong <b><i><u>AND YOU CAN PROVE IT</u></i></b> then please submit an issue <b><a href=https://github.com/FrankTheBoxMonster/LoL-Damage/issues/new>HERE</a></b> with details.";
+
+
 var filterHeaderDiv = document.createElement("div");
 document.body.appendChild(filterHeaderDiv);
 filterHeaderDiv.style.marginTop = "-10px";
